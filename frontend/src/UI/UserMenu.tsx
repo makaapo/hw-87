e39624a4774cc4bcccb4587e099ad3aab6505432
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-09-19 06:22:16 +0600

    Сделал api для юзера так же как и на прошлых заданиях, добавил logout в навигации юзера, вся валидация и очистка аккаунта работают

--- frontend/src/UI/UserMenu.tsx
+++ frontend/src/UI/UserMenu.tsx
@@ -2,12 +2,15 @@ import React, {useState} from 'react';
 import {Button, Grid, Menu, MenuItem} from '@mui/material';
 import RestoreIcon from '@mui/icons-material/Restore';
 import {User} from '../types.ts';
+import {useAppDispatch} from '../app/hooks.ts';
+import {logout} from '../features/User/usersThunks.ts';
 
 interface Props {
   user: User;
 }
 
 const UserMenu: React.FC<Props> = ({user}) => {
+  const dispatch = useAppDispatch();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const isOpen= Boolean(anchorEl)
 
@@ -18,15 +21,20 @@ const UserMenu: React.FC<Props> = ({user}) => {
   const handleClose = () => {
     setAnchorEl(null);
   }
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <Grid item>
       <Button onClick={handleClick} color="inherit">
         Hello, {user.username}!
       </Button>
       <Menu open={isOpen} anchorEl={anchorEl} onClose={handleClose} keepMounted>
-        <MenuItem>
+        <MenuItem onClick={handleLogout}>
           <RestoreIcon sx={{mr: 2}} />
-          Log out
+          Logout
         </MenuItem>
       </Menu>
     </Grid>
